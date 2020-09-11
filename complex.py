35629c89d522a2f5caee8f077c07013a87a39b11
Leonardo Bruno Federico <70327142+lbfederico@users.noreply.github.com>
2020-09-11 09:33:14 -0300

    Update complex.py

--- complex.py
+++ complex.py
@@ -5,18 +5,18 @@ prot = sys.argv[1]
 lig = sys.argv[2]
 
 
-# Abre arquivo e lê prot
+# Open file and read prot
 file = open(str(prot), 'r')
 prot = file.readlines()
 
-#Criar complex, parte1 -> prot sem box vector (only_prot)
+#Build complex, part1 -> prot without box vector (only_prot)
 file = open('complex.gro', 'a+')
 only_prot = prot[2:-1]
 file.writelines([item for item in only_prot])
 box_vector = prot[-1]
 file.close()
 
-#incluir ligante sem cabeçalho nem box vector -> incluir box vector
+#Add ligand without title and box vector -> include box vector
 #Abre arquivo e lê lig e complex
 file = open(str(lig), 'r')
 lig = file.readlines()
@@ -27,13 +27,13 @@ file.writelines([item for item in lig_only])
 file.writelines([item for item in box_vector])
 file.close()
 
-#Verificar o numero de linhas (cont) e a linha2 - como esta sem cabeçalho o numero de moleculas é cont -1
+#Check the number of lines  (cont) and line2 - (as this without title the number of molecules is cont -1)
 file = open('complex.gro', 'r')
 line = file.readlines()
 cont = len(line)
 file.close()
 
-#Substituir o numero de linhas e o nome inicial
+#Replace the number of lines and the initial name
 file = open('complex.gro', 'w')
 file.write('complex' + '\n')
 file.write(str(cont -1)+ '\n')
