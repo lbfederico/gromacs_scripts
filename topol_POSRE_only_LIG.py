2edab6232b087431bdaa2020c34257ba26c3611b
Leonardo Bruno Federico <70327142+lbfederico@users.noreply.github.com>
2020-09-12 10:46:07 -0300

    Update topol_POSRE_only_LIG.py

--- topol_POSRE_only_LIG.py
+++ topol_POSRE_only_LIG.py
@@ -1,25 +1,30 @@
 import sys
 
-#Função para encontrar numero da linha que contém o argumemnto desejado
 def get_line(txt):
     file = open('topol.top', encoding='utf8')
-    for line_num, value in enumerate(file, 1): #le todas as linhas e valores do file começando como 1
-        if txt in value:                       #qdo encontra o 'txt' retorna o valor da linha
+    for line_num, value in enumerate(file, 1): 
+        if txt in value:                       
             return line_num
+if len(sys.argv) < 2:
+    print('ERROR: put lig name without extension')
 
-#Argumentos
-lig = sys.argv[1]
+elif len(sys.argv) > 2:
+    print('ERROR: put only lig name without extension')
 
-#txt para encontrar o numero da linha e add após ou antes os argumentos .prm e .itp
-x = str('; Include water topology')
-y = str(f'; Ligand position restraints\n'
-        f'#ifdef POSRES\n'
-        f'#include "posre_{lig}.itp"'
-        f'\n#endif\n\n')
+else:
+  
+    lig = sys.argv[1]
 
-#Abrir o topol.top -> Ler as linhas a add o argumento .prm
-file = open('topol.top', 'r+', encoding='utf8')
-line = file.readlines()
-line.insert(get_line(x)-1, y)
-file.seek(0)
-file.writelines(line)
+    
+    x = str('; Include water topology')
+    y = str(f'; Ligand position restraints\n'
+            f'#ifdef POSRES\n'
+            f'#include "posre_{lig}.itp"'
+            f'\n#endif\n\n')
+
+
+    file = open('topol.top', 'r+', encoding='utf8')
+    line = file.readlines()
+    line.insert(get_line(x)-1, y)
+    file.seek(0)
+    file.writelines(line)
