bbca0b4d36daa1e5e6f8ab24b7b2876657a7f79c
Leonardo Bruno Federico <70327142+lbfederico@users.noreply.github.com>
2020-09-12 10:34:58 -0300

    Update topol_only_LIG.py

--- topol_only_LIG.py
+++ topol_only_LIG.py
@@ -1,34 +1,41 @@
 import sys
 
-#Função para encontrar numero da linha que contém o argumemnto desejado
 def get_line(txt):
     file = open('topol.top', encoding='utf8')
-    for line_num, value in enumerate(file, 1): #le todas as linhas e valores do file começando como 1
-        if txt in value:                       #qdo encontra o 'txt' retorna o valor da linha
+    for line_num, value in enumerate(file, 1): 
+        if txt in value:                       
             return line_num
 
-#Argumento
-lig = sys.argv[1]
 
+        
+if len(sys.argv) < 2:
+    print('ERROR: put lig name without extension')
 
-x = str('#include "./charmm36-mar2019.ff/forcefield.itp"')
-y = str('; Include Position restraint file')
-z = str('Protein             1')
+elif len(sys.argv) > 2:
+    print('ERROR: put only lig name without extension')
 
-file = open('topol.top', 'r+', encoding='utf8')
-line = file.readlines()
-line.insert(get_line(x),f'\n; Include ligand parameters\n#include "{lig}.prm"\n')
-file.seek(0)
-file.writelines(line)
+else:
+    
+    lig = sys.argv[1]
 
-file = open('topol.top', 'r+', encoding='utf8')
-line = file.readlines()
-line.insert(get_line(y)+ 4,f'; Include ligand topology\n#include "{lig}.itp"\n\n')
-file.seek(0)
-file.writelines(line)
+    x = str('#include "./charmm36-mar2019.ff/forcefield.itp"')
+    y = str('; Include Position restraint file')
+    z = str('Protein             1')
 
-file = open('topol.top', 'r+', encoding='utf8')
-line = file.readlines()
-line.insert(get_line(z),f'\n{lig}                 1')
-file.seek(0)
-file.writelines(line)
+    file = open('topol.top', 'r+', encoding='utf8')
+    line = file.readlines()
+    line.insert(get_line(x),f'\n; Include ligand parameters\n#include "{lig}.prm"\n')
+    file.seek(0)
+    file.writelines(line)
+
+    file = open('topol.top', 'r+', encoding='utf8')
+    line = file.readlines()
+    line.insert(get_line(y)+ 4,f'; Include ligand topology\n#include "{lig}.itp"\n\n')
+    file.seek(0)
+    file.writelines(line)
+
+    file = open('topol.top', 'r+', encoding='utf8')
+    line = file.readlines()
+    line.insert(get_line(z),f'\n{lig}                 1')
+    file.seek(0)
+    file.writelines(line)
