02dedea42525860304cd83446825480b6cba0b12
Leonardo Bruno Federico <70327142+lbfederico@users.noreply.github.com>
2020-12-19 14:15:58 -0300

    Update analises_gmx.py

--- analises_gmx.py
+++ analises_gmx.py
@@ -1,9 +1,11 @@
 import os
 import sys
 
-#argments(gmx flags)
 
+# lists only directories 
 d = list(filter(os.path.isdir, os.listdir()))
+
+#argments(gmx options and flags)
 cmd1 = ('echo 1 0 | gmx trjconv -s md_sv_mdh.tpr -f md_sv_mdh.xtc -o md_center.xtc -center -pbc mol -ur compact')
 cmd1_gp = ('echo 1 0 | gmx trjconv -s md_gp_mdh.tpr -f md_gp_mdh.xtc -o md_center.xtc -center -pbc mol -ur compact')
 cmd2 = ('echo 4 0 | gmx trjconv -s md_sv_mdh.tpr -f md_center.xtc -o md_fit.xtc -fit rot+trans')
@@ -14,20 +16,23 @@ cmd5 = ('echo 1 | gmx sasa -s md_sv_mdh.tpr -f md_fit.xtc -o sasa.xvg')
 cmd6 = ('echo 1 | gmx gyrate -s md_sv_mdh.tpr -f md_fit.xtc -o gyrate.xvg')
 
 
+#check the number of arguments (only 1 is allowed)
 if len(sys.argv) != 2:
     print('ERROR: put sv_all, sv_trj or gp_trj')
 
+# to all analyses
+
 elif sys.argv[1] == str('sv_all'):
     for x in d:
-        path = os.path.abspath(x)
-        os.chdir(path)
+        path = os.path.abspath(x) #directory path
+        os.chdir(path)            #change directory
         os.system(cmd1)
         os.system(cmd2)
         os.system(cmd3)
         os.system(cmd4)
         os.system(cmd5)
         os.system(cmd6)
-        os.chdir(os.path.join('../../'))
+        os.chdir(os.path.join('../../')) #move to two directories below (I put my results in results>gp>ligand1 and script file is in results)
 
 elif sys.argv[1] == str('sv_trj'):
     for x in d:
